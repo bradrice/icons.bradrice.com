2e94a5c5e3cfae62d759212db4dbcbcb51c9f65b
Brad Rice <bradrice1@gmail.com>
2024-07-24 12:09:01 -0400

    initial init from rivendell

---
+++ icons/settings/production.py
@@ -0,0 +1,21 @@
+import os
+from .base import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEBUG = False
+
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
+
+ALLOWED_HOSTS = ['icons.bradrice.com']
+ALLOWED_HOSTS = [
+  'localhost',
+  '127.0.0.1',
+  '45.56.110.221',
+  'icons.bradrice.com']
+
+try:
+    from .local import *
+except ImportError:
+    pass
