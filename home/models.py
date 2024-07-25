16d67be1e1c181389a067b10ea4f59fb6fd2fd97
Brad Rice <bradrice1@gmail.com>
2024-07-24 22:30:10 -0400

    added postcard to cover

--- home/models.py
+++ home/models.py
@@ -2,14 +2,21 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
-
+from wagtail.images import get_image_model
 
 class HomePage(Page):
     max_count = 1
     subtitle = models.CharField(max_length=255, blank=True)
     body = RichTextField(blank=True)
-
+    image = models.ForeignKey(
+        get_image_model(),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
         FieldPanel('body'),
+        FieldPanel('image'),
     ]
